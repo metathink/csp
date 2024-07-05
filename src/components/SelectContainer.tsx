ea9c0530aa4d51416d237f0d5bbdc3964017d3ff
metathink <jmpmetaimpthink@gmail.com>
2024-07-05 17:23:41 +0900

    fixed

--- src/components/SelectContainer.tsx
+++ src/components/SelectContainer.tsx
@@ -1,11 +1,9 @@
 import React from "react"
-import initialize from "../util/initialize"
 
 export default ({ tab, setTab }: { tab: string, setTab: React.Dispatch<React.SetStateAction<string>> }) => {
 
     const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
         setTab(e.target.value)
-        initialize()
     }
 
     return (
