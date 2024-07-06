16e1de6fff3f0f600b4469f4b901df5d9d964423
metathink <jmpmetaimpthink@gmail.com>
2024-07-06 12:52:16 +0900

    result change text

--- src/components/unit/Result.tsx
+++ src/components/unit/Result.tsx
@@ -13,8 +13,8 @@ export default ({ result }: {
             {result.cost.length > 1 && (
                 <Card style={{ margin: 8, padding: '10px', border: '1px solid #ddd', borderRadius: '5px', backgroundColor: '#f9f9f9' }}>
                     <Typography.Title level={3}> 結果 </Typography.Title>
-                    <p>{result.cost}</p>
-                    <p>1円あたりの価値 - A: {result.quality.A} 単位/円, B: {result.quality.B} 単位/円</p>
+                    <Typography.Text strong>{result.cost}</Typography.Text>
+                    <Typography.Text strong>1円あたりの価値 - A: {result.quality.A} 単位/円, B: {result.quality.B} 単位/円</Typography.Text>
                 </Card>
             )}
         </>
